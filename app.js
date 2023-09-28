84ebbff7a2eb22505bc6cfbc51508d75cee6f17e
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-09-27 19:16:32 -0700

    Created front end for todo app

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res)=>{
-    res.send("HOME PAGE")
+    res.render("home.ejs");
 });
 
 app.listen(PORT, () =>{
-    console.log(`App running on: localhost:${PORT}`)
+    console.log(`App running on: localhost:${PORT}`);
 });
 
